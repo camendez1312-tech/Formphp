8d80b11c34eef3bb8ad815059e262942dd7e6580
camendez1312-tech <camendez1312@gmail.com>
2025-10-02 10:28:13 -0500

    Update informacion.php

--- informacion.php
+++ informacion.php
@@ -1,14 +1,20 @@
 <?php
-$nombre    = $_POST['nombre'];
-$edad      = $_POST['edad'];
-$direccion = $_POST['direccion'];
-$salario   = $_POST['salario'];
-$correo    = $_POST['correo'];
-$cargo     = $_POST['cargo'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nombre    = $_POST['nombre'];
+    $edad      = $_POST['edad'];
+    $direccion = $_POST['direccion'];
+    $correo    = $_POST['correo'];
+    $cargo     = $_POST['cargo'];
+    $salario   = $_POST['salario'];
 
-echo "<h2>Información recibida</h2>";
-echo "El empleado <strong>$nombre</strong> tiene <strong>$edad</strong> años.<br>";
-echo "Vive en <strong>$direccion</strong>.<br>";
-echo "Su cargo es <strong>$cargo</strong> y su salario es <strong>$$salario</strong>.<br>";
-echo "Correo de contacto: <strong>$correo</strong>.";
+    echo "<h2>Información recibida</h2>";
+    echo "<table border='1' cellpadding='5'>";
+    echo "<tr><td><strong>Nombre</strong></td><td>$nombre</td></tr>";
+    echo "<tr><td><strong>Edad</strong></td><td>$edad</td></tr>";
+    echo "<tr><td><strong>Dirección</strong></td><td>$direccion</td></tr>";
+    echo "<tr><td><strong>Correo electrónico</strong></td><td>$correo</td></tr>";
+    echo "<tr><td><strong>Cargo</strong></td><td>$cargo</td></tr>";
+    echo "<tr><td><strong>Salario</strong></td><td>$$salario</td></tr>";
+    echo "</table>";
+}
 ?>
