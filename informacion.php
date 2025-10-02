cdbbf6b854d06abd0ea3821365d9724ee35e8efa
camendez1312-tech <camendez1312@gmail.com>
2025-10-02 09:28:50 -0500

    Update informacion.php

--- informacion.php
+++ informacion.php
@@ -1,12 +1,14 @@
 <?php
-$nombre = $_POST ['nombre'];
-$edad = $_POST ['edad'];
-$direccion = $_POST ['direccion'];
-$salario = $_POST ['salario'];
-$correo = $_POST ['correo'];
+$nombre    = $_POST['nombre'];
+$edad      = $_POST['edad'];
+$direccion = $_POST['direccion'];
+$salario   = $_POST['salario'];
+$correo    = $_POST['correo'];
+$cargo     = $_POST['cargo'];
 
-echo "<h2>Informacion recibida</h2>";
-echo "El empleado $nombre tiene $edad años.<br>";
-echo "Vive en $direccion y su salario es $salario.";
-echo "correo $correo.";
+echo "<h2>Información recibida</h2>";
+echo "El empleado <strong>$nombre</strong> tiene <strong>$edad</strong> años.<br>";
+echo "Vive en <strong>$direccion</strong>.<br>";
+echo "Su cargo es <strong>$cargo</strong> y su salario es <strong>$$salario</strong>.<br>";
+echo "Correo de contacto: <strong>$correo</strong>.";
 ?>
